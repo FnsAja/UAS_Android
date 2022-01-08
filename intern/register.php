e577bd5917d552ce13d4d7e5a17566884a85faf4
bbyboy <martinusdx001@gmail.com>
2022-01-08 22:19:17 +0700

    AMAN BOS

--- intern/register.php
+++ intern/register.php
@@ -11,10 +11,11 @@ $confirm_password = trim($_POST["confirm_password"]); //menghilangkan spasi di c
 $address = trim($_POST["address"]); //menghilangkan spasi di address
 $email = trim($_POST["email"]); //menghilangkan spasi di email
 $phone = trim($_POST["phone"]); //menghilangkan spasi di phone
+$about =($_POST["about"]); //post about ke database
 $division = trim($_POST["division"]); //menghilangkan spasi di division 
-$status = trim($_POST["status"]); //menghilangkan spasi di status
+$performance = trim($_POST["performance"]); //menghilangkan spasi di status
 
-if ((empty($fullname)) || (empty($username)) || (empty($password)) || (empty($address)) || (empty($email)) || (empty($phone)) || (empty($division)) || (empty($status))) {
+if ((empty($fullname)) || (empty($username)) || (empty($password)) || (empty($address)) || (empty($email)) || (empty($phone)) || (empty($about)) || (empty($division)) || (empty($performance))) {
 	$response = new user();
 	$response->success = 0;
 	$response->message = "Field tidak boleh kosong"; //fungsi untuk cek apabila inputan diantara fullname, username, password, email, phone, division, status ada yang kosong.
@@ -30,18 +31,42 @@ if ((empty($fullname)) || (empty($username)) || (empty($password)) || (empty($ad
 		$num_rows = mysqli_num_rows(mysqli_query($con, "SELECT * FROM users WHERE username='".$username."'")); //select database sesuai dengan username
 
         if ($num_rows == 0){
-		$query = mysqli_query($con, "INSERT INTO users (nama, username, password, alamat, email, notelp, divisi, status) VALUES ('".$fullname."','".$username."', '".$password."', '".$address."', '".$email."', '".$phone."', '".$division."', '".$status."')");
-			if ($query){				//query untuk push data kedalam database.
+			$username_pattern = "/^(?=.{6,12}$)(?![_.])[a-zA-Z0-9]+(?<![_.])$/"; 
+			$email_pattern = "/^[-_.a-zA-z\d]+@[a-z]+(.[a-z]+)*(.[a-z]{2,3})$/";
+			$password_pattern = "/^(?=.*[A-Za-z])[A-Za-z\d]{6,10}$/";
+			if(preg_match($username_pattern, $username)){
+				if(preg_match($email_pattern, $email)){
+					if(preg_match($password_pattern, $password)){
+						$query = mysqli_query($con, "INSERT INTO users (nama, username, password, alamat, email, notelp, divisi, performance, about) VALUES ('".$fullname."','".$username."', '".$password."', '".$address."', '".$email."', '".$phone."', '".$division."', '".$performance."', '".$about."')");
+							if ($query){				//query untuk push data kedalam database.
+							$response = new user();
+							$response->success = 1;
+							$response->message = "Register berhasil";
+							die(json_encode($response)); //registrasi berhasil dan data masuk kedalam database
+							}else{
+								$response = new user();
+								$response->success = 0;
+								$response->message = "Username sudah terdaftar";
+								die(json_encode($response)); //username telah terdaftar
+							}
+					}else {
+						$response = new user();
+						$response->success = 0;
+						$response->message = "Password salah, mohon untuk tidak melebihi 8 karakter dan kurang dari 6 karakter";
+						die(json_encode($response));
+					}
+				}else {
+					$response = new user();
+					$response->success = 0;
+					$response->message = "Format Email salah, mohon cek ulang";
+					die(json_encode($response));
+				}					
+			}else{
 				$response = new user();
-				$response->success = 1;
-				$response->message = "Register berhasil";
-				die(json_encode($response)); //registrasi berhasil dan data masuk kedalam database
+				$response->success = 0;
+				$response->message = "Username harus memiliki 6 - 12 karakter dan tidak mengandung simbol";
+				die(json_encode($response));
 			}
-		} else {
-			$response = new user();
-			$response->success = 0;
-			$response->message = "Username sudah terdaftar";
-			die(json_encode($response)); //username telah terdaftar
 		}
 	}
 }
